646f9a8d6959ec68335551dd8754a392f37d01e3
Nayara <nayaramaisa.stn@gmail.com>
2025-02-06 15:42:42 -0300

    last change

--- src/Notas.java
+++ src/Notas.java
@@ -12,16 +12,23 @@ public class Notas {
     }
 
     public static void mediaDeNotas() {
-        float nota1 = 5;
-        float nota2 = 7;
+        float nota1 = 2;
+        float nota2 = 1;
         float nota3 = 9;
         float nota4 = 3;
-
 
         media = (nota1 + nota2 + nota3 + nota4) / 4;
     }
 
     public static void exibirMedia() {
         System.out.println("A média é: " + media);
+
+        if (media >= 7) {
+            System.out.println("Aprovado!");
+        } else if (media >= 5) {
+            System.out.println("Em recuperação.");
+        } else {
+            System.out.println("Reprovado.");
+        }
     }
 }
